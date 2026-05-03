ed06c11deee0dd1b17f5f38664b9a3067872adf2
Dhanraj1019 <choudharydhanraj714@gmail.com>
2026-05-03 11:28:34 +0530

    add sesstion of login for 1 day

--- server.js
+++ server.js
@@ -102,6 +102,9 @@ app.use(session({
     secret: secretkey,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 // 1 day
+    }
 }))
 
 app.use(flash())
